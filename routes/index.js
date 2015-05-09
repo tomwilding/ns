24c0b75dfc3bac7418f75dc7a22a726db634df32
thomas.wilding <thomas.wilding@accenture.com>
2015-05-09 17:45:15 +0100

    installed pg and updated to run hk

--- routes/index.js
+++ routes/index.js
@@ -7,16 +7,16 @@ router.post('/submitCustomerData', function(req, res, next) {
   // fs.appendFile('customerSignup.txt', JSON.stringify(req.body) + ',', function (err) {
   //   if (err) return console.log(err);
   // });
-	var conString = "pg://postgres:postgres@localhost:5432/customers";
-	var client = new pg.Client(conString);
+	console.log("connecting");
+	var client = new Client(process.env.DATABASE_URL);
 	client.connect();
-
+	console.log("connected");
 	var insert_records = function(req, res) {
 		// Creat table and insert 2 records into it
 		client.query("CREATE TABLE IF NOT EXISTS customers(firstname varchar(64), lastname varchar(64))");
 		client.query("INSERT INTO customers(firstname, lastname) values($1, $2)", ['Tinniam', 'Ganesh']);
 		client.query("INSERT INTO customers(firstname, lastname) values($1, $2)", ['Anand', 'Karthik']);
-	});
+	};
 });
 
 module.exports = router;
