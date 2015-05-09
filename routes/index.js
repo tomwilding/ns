aea5c8bc01ff22c00551a4253f79f560af2dd2c3
thomas.wilding <thomas.wilding@accenture.com>
2015-05-09 17:01:34 +0100

    postgresql

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,20 @@ var router = express.Router();
 
 /* POST customer data */
 router.post('/submitCustomerData', function(req, res, next) {
-  fs = require('fs');
-  fs.appendFile('customerSignup.txt', JSON.stringify(req.body) + ',', function (err) {
-    if (err) return console.log(err);
-  });
+  // fs = require('fs');
+  // fs.appendFile('customerSignup.txt', JSON.stringify(req.body) + ',', function (err) {
+  //   if (err) return console.log(err);
+  // });
+	var conString = "pg://postgres:postgres@localhost:5432/customers";
+	var client = new pg.Client(conString);
+	client.connect();
+
+	var insert_records = function(req, res) {
+		// Creat table and insert 2 records into it
+		client.query("CREATE TABLE IF NOT EXISTS customers(firstname varchar(64), lastname varchar(64))");
+		client.query("INSERT INTO customers(firstname, lastname) values($1, $2)", ['Tinniam', 'Ganesh']);
+		client.query("INSERT INTO customers(firstname, lastname) values($1, $2)", ['Anand', 'Karthik']);
+	});
 });
 
 module.exports = router;
