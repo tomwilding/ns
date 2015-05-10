ab732b75b2e8f79ae7977def2c7d580a608b1a5e
thomas.wilding <thomas.wilding@accenture.com>
2015-05-10 02:26:24 +0100

    pg config

--- app.js
+++ app.js
@@ -4,11 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var pg = require('pg');
-
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-});
-
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
