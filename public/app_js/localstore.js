282119fed7b0367e0dce4c6a99879be9cc2338af
thomas.wilding <thomas.wilding@accenture.com>
2015-05-10 02:13:37 +0100

    changed to port

--- public/app_js/localstore.js
+++ public/app_js/localstore.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
   						email: $("#email").val(),
   						number: $("#number").val()
   					};
-  		$.post("http://0.0.0.0:80/submitCustomerData", details, function(data){});
+  		var port = process.env.PORT || '3000';
+  		$.post("http://0.0.0.0" + port + "/submitCustomerData", details, function(data){});
 	});
 });
