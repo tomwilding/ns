7748ce13d3e62e46702f67571f1e78fb8f1838d0
thomas.wilding <thomas.wilding@accenture.com>
2015-05-10 01:36:04 +0100

    removed js

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,12 @@ router.post('/submitCustomerData', function(req, res, next) {
   // fs.appendFile('customerSignup.txt', JSON.stringify(req.body) + ',', function (err) {
   //   if (err) return console.log(err);
   // });
-	console.log("connecting");
-	pg.connect(process.env.DATABASE_URL, function(err, client) {
-		client.query("CREATE TABLE IF NOT EXISTS customers(firstname varchar(64), lastname varchar(64))");
-		client.query("INSERT INTO customers(firstname, lastname) values($1, $2)", ['Tinniam', 'Ganesh']);
-		client.query("INSERT INTO customers(firstname, lastname) values($1, $2)", ['Anand', 'Karthik']);
-	};
+	// console.log("connecting");
+	// pg.connect(process.env.DATABASE_URL, function(err, client) {
+	// 	client.query("CREATE TABLE IF NOT EXISTS customers(firstname varchar(64), lastname varchar(64))");
+	// 	client.query("INSERT INTO customers(firstname, lastname) values($1, $2)", ['Tinniam', 'Ganesh']);
+	// 	client.query("INSERT INTO customers(firstname, lastname) values($1, $2)", ['Anand', 'Karthik']);
+	// };
 });
 
 module.exports = router;
