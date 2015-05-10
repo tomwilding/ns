624157296ced9672ee006542d5a20decfc087a7e
thomas.wilding <thomas.wilding@accenture.com>
2015-05-10 02:35:44 +0100

    pg config

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 
 var pg = require('pg'); 
 var connectionString = "postgres://mrvgznqwjrhmfo:0O7cEIHVZ1kXXIHyLQ1nvcifwK@ec2-23-21-96-129.compute-1.amazonaws.com:5432/dbo8l3cdcfpk01";
-pg.connect(connectionString, function(err, client) {
+pg.connect(process.env.DATABASE_URL, function(err, client) {
   client.query("CREATE TABLE IF NOT EXISTS customers(firstname varchar(64), lastname varchar(64))");
 };
 
