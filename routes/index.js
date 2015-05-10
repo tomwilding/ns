8f9e17ac31c124788d0d5edae028b20e241bcf88
thomas.wilding <thomas.wilding@accenture.com>
2015-05-10 01:50:35 +0100

    renamed con

--- routes/index.js
+++ routes/index.js
@@ -10,9 +10,6 @@ router.post('/submitCustomerData', function(req, res, next) {
 	console.log("connecting");
 	var connectionString = "postgres://mrvgznqwjrhmfo:0O7cEIHVZ1kXXIHyLQ1nvcifwK@ec2-23-21-96-129.compute-1.amazonaws.com:5432/dbo8l3cdcfpk01"
 	pg.connect(connectionString, function(err, client) {
-		client.query("CREATE TABLE IF NOT EXISTS customers(firstname varchar(64), lastname varchar(64))");
-		client.query("INSERT INTO customers(firstname, lastname) values($1, $2)", ['Tinniam', 'Ganesh']);
-		client.query("INSERT INTO customers(firstname, lastname) values($1, $2)", ['Anand', 'Karthik']);
 	};
 });
 
