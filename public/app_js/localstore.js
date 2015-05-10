14af06309121e7ba508c685b4d9c38fbd64fd76e
thomas.wilding <thomas.wilding@accenture.com>
2015-05-10 01:55:54 +0100

    changed to 0.0.0.0

--- public/app_js/localstore.js
+++ public/app_js/localstore.js
@@ -6,6 +6,6 @@ $(document).ready(function() {
   						email: $("#email").val(),
   						number: $("#number").val()
   					};
-  		$.post("http://localhost:3000/submitCustomerData", details, function(data){});
+  		$.post("http://0.0.0.0/submitCustomerData", details, function(data){});
 	});
 });
