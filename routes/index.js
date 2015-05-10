2309337430b6c33d2e84784ad3db7d56d8d3568d
thomas.wilding <thomas.wilding@accenture.com>
2015-05-10 01:42:22 +0100

    config

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,13 @@ router.post('/submitCustomerData', function(req, res, next) {
   // fs.appendFile('customerSignup.txt', JSON.stringify(req.body) + ',', function (err) {
   //   if (err) return console.log(err);
   // });
-	// console.log("connecting");
-	// pg.connect(process.env.DATABASE_URL, function(err, client) {
-	// 	client.query("CREATE TABLE IF NOT EXISTS customers(firstname varchar(64), lastname varchar(64))");
-	// 	client.query("INSERT INTO customers(firstname, lastname) values($1, $2)", ['Tinniam', 'Ganesh']);
-	// 	client.query("INSERT INTO customers(firstname, lastname) values($1, $2)", ['Anand', 'Karthik']);
-	// };
+	console.log("connecting");
+	var connectionString = "postgres://mrvgznqwjrhmfo:0O7cEIHVZ1kXXIHyLQ1nvcifwK@ec2-23-21-96-129.compute-1.amazonaws.com:5432:/dbo8l3cdcfpk01"
+	pg.connect(connectionString, function(err, client) {
+		client.query("CREATE TABLE IF NOT EXISTS customers(firstname varchar(64), lastname varchar(64))");
+		client.query("INSERT INTO customers(firstname, lastname) values($1, $2)", ['Tinniam', 'Ganesh']);
+		client.query("INSERT INTO customers(firstname, lastname) values($1, $2)", ['Anand', 'Karthik']);
+	};
 });
 
 module.exports = router;
